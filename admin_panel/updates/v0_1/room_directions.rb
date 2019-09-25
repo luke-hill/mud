dd9f3a62452958f5787667af29feb04a3c990b19
Luke Hill <lukehill_uk@hotmail.com>
2019-09-25 16:30:26 +0200

    Add v0.1 and v0.2 to seeds files, to allow better tracking of data

---
+++ admin_panel/updates/v0_1/room_directions.rb
@@ -0,0 +1,26 @@
+# V0.1 Newtown and first Dungeon
+
+require_relative '../../room/directions'
+
+ap = AdminPanel::Room::Directions
+
+ap.seed(id: 1, north: 9, east: 2, west: 8)
+ap.seed(id: 2, north: 3, south: 21, west: 1)
+ap.seed(id: 3, north: 4, south: 2, west: 9)
+ap.seed(id: 4, south: 3, west: 5)
+ap.seed(id: 5, north: 10, south: 9, east: 4, west: 6)
+ap.seed(id: 6, south: 7, east: 5)
+ap.seed(id: 7, north: 6, south: 8, east: 9)
+ap.seed(id: 8, north: 7, east: 1)
+ap.seed(id: 9, north: 5, south: 1, east: 3, west: 7)
+ap.seed(id: 10, north: 11, south: 5)
+ap.seed(id: 11, north: 12, south: 10)
+ap.seed(id: 12, south: 11, east: 13)
+ap.seed(id: 13, east: 14, west: 12)
+ap.seed(id: 14, east: 15, west: 13)
+ap.seed(id: 15, east: 16, west: 14)
+ap.seed(id: 16, east: 17, west: 15)
+ap.seed(id: 17, east: 18, west: 16)
+ap.seed(id: 18, east: 19, west: 17)
+ap.seed(id: 19, east: 20, west: 18)
+ap.seed(id: 20, west: 19)
---
+++ admin_panel/updates/v0_2/room_directions.rb
@@ -0,0 +1,20 @@
+# V0.2 Newtown district directions
+
+require_relative '../../room/directions'
+
+ap = AdminPanel::Room::Directions
+
+ap.seed(id: 21, north: 2, south: 22)
+ap.seed(id: 22, north: 21, south: 23)
+ap.seed(id: 23, north: 22, south: 24, east: 34)
+ap.seed(id: 24, north: 23, south: 25)
+ap.seed(id: 25, north: 24, south: 26)
+ap.seed(id: 26, north: 25, south: 27, east: 32, west: 33)
+ap.seed(id: 27, north: 26, south: 28)
+ap.seed(id: 28, north: 27, south: 29, east: 30, west: 31)
+ap.seed(id: 29, north: 28)
+ap.seed(id: 30, west: 28)
+ap.seed(id: 31, east: 28)
+ap.seed(id: 32, west: 26)
+ap.seed(id: 33, east: 26)
+ap.seed(id: 34, west: 23)
