ebaad278b3400dae8b88a0002173c5683c05f5a5
Luke Hill <lukehill_uk@hotmail.com>
2019-10-08 15:37:57 +0200

    Add v0.1 room location info

---
+++ admin_panel/room/locations.rb
@@ -0,0 +1,11 @@
+module AdminPanel
+  module Room
+    class Locations < AdminPanel::Base
+      private
+
+      def yml_file_location
+        '/home/luke/Code/mud/data/rooms/locations.yml'
+      end
+    end
+  end
+end
---
+++ admin_panel/updates/v0_1/room_locations.rb
@@ -0,0 +1,16 @@
+# V0.1 Locations
+
+ap = AdminPanel::Room::Directions
+
+ap.seed(id: 'newtown_town_square', town: 'Newtown', area: 'Town Square')
+ap.seed(id: 'newtown_travelling_merchants_shop', town: 'Newtown', area: 'Travelling Merchants Shop')
+ap.seed(id: 'newtown_training_area', town: 'Newtown', area: 'Training Area')
+ap.seed(id: 'newtown_basic_training_guild', town: 'Newtown', area: 'Basic Training Guild')
+ap.seed(id: 'newtown_gatehouse', town: 'Newtown', area: 'Gatehouse')
+ap.seed(id: 'newtown_outskirts', town: 'Newtown', area: 'Outskirts')
+ap.seed(id: 'newtown_residential_district', town: 'Newtown', area: 'Residential District')
+ap.seed(id: 'newtown_residential_district_crossroads', town: 'Newtown', area: 'Residential District Crossroads')
+ap.seed(id: 'newtown_house', town: 'Newtown', area: 'House')
+ap.seed(id: 'newtown_bank', town: 'Newtown', area: 'Bank')
+ap.seed(id: 'forest_path', area: 'Forest Path')
+ap.seed(id: 'barracks', area: 'Barracks')
