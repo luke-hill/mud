fb61dc6725be7fd6878f2336a57ac1c065fd5652
Luke Hill <lukehill_uk@hotmail.com>
2019-10-08 11:18:00 +0200

    DRY up seeder so the only varying part is the file location (Input is just a hash of options which is not validated other than containing an id)

--- admin_panel/base.rb
+++ admin_panel/base.rb
@@ -25,12 +25,28 @@ module AdminPanel
       @id ||= options.delete(:id)
     end
 
-    def save
-      File.write(yml_file_location, yml_file.to_yaml)
+    def update
+      puts "Data to be seeded: #{options}"
+      puts "Existing Data: #{data}"
+
+      options.each do |key, value|
+        data[key.to_s] = value
+      end
+
+      yml_file[id] = data
+      puts "New UPDATED Data: #{data}"
+    end
+
+    def data
+      @data ||= yml_file[id] || {}
     end
 
     def yml_file
       @yml_file ||= YAML.load_file(yml_file_location)
+    end
+
+    def save
+      File.write(yml_file_location, yml_file.to_yaml)
     end
   end
 end
--- admin_panel/room/directions.rb
+++ admin_panel/room/directions.rb
@@ -1,25 +1,7 @@
-require 'yaml'
-
 module AdminPanel
   module Room
     class Directions < AdminPanel::Base
       private
-
-      def directions
-        @directions ||= yml_file[id] || {}
-      end
-
-      def update
-        puts "Data to be seeded: #{options}"
-        puts "Existing Data: #{directions}"
-
-        options.each do |key, value|
-          directions[key.to_s] = value
-        end
-
-        yml_file[id] = directions
-        puts "New UPDATED Data: #{directions}"
-      end
 
       def yml_file_location
         '/home/luke/Code/mud/data/rooms/directions.yml'
