8265b82c3edeb4244b135a1459f825cd968f8fcc
Luke Hill <lukehill_uk@hotmail.com>
2019-10-15 18:31:13 +0200

    Move out all confirmed working logic into base character class to thin out the fighter class

---
+++ app/classes/base.rb
@@ -0,0 +1,52 @@
+module MUD
+  module Classes
+    class Base
+      attr_reader :attributes, :equipment
+      private :attributes, :equipment
+
+      attr_accessor :current_room, :rooms_visited
+
+      # These are all the accessors for the individual attributes which allow
+      # us to read and write to them whilst also logging our requests for
+      # maximum traceability
+      include MUD::Helpers::AttributeAccessors
+
+      def initialize
+        @attributes = starting_attributes
+        @current_room = starting_room
+        @equipment = starting_equipment
+        set_rooms_visited_to_blank
+      end
+
+      def move(direction)
+        Logger.debug("Attempting to move #{direction}")
+        MUD::Movement::Move.send(direction)
+      end
+
+      def connected_rooms
+        current_room.connected_rooms
+      end
+
+      private
+
+      def starting_equipment
+        {
+          weapon: 'fists',
+          armor: 'unarmored'
+        }
+      end
+
+      def starting_room
+        MUD::Rooms::Room.new(starting_room)
+      end
+
+      def starting_room_id
+        1
+      end
+
+      def set_rooms_visited_to_blank
+        @rooms_visited ||= {}
+      end
+    end
+  end
+end
--- app/classes/fighter.rb
+++ app/classes/fighter.rb
@@ -1,32 +1,6 @@
 module MUD
   module Classes
-    class Fighter
-      attr_reader :attributes, :equipment
-      private :attributes, :equipment
-
-      attr_accessor :current_room, :rooms_visited
-
-      # These are all the accessors for the individual attributes which allow
-      # us to read and write to them whilst also logging our requests for
-      # maximum traceability
-      include MUD::Helpers::AttributeAccessors
-
-      def initialize
-        @attributes = starting_attributes
-        @current_room = MUD::Rooms::Room.new(starting_room)
-        set_rooms_visited_to_blank
-        @equipment = starting_equipment
-      end
-
-      def connected_rooms
-        current_room.connected_rooms
-      end
-
-      def move(direction)
-        Logger.debug("Attempting to move #{direction}")
-        MUD::Movement::Move.send(direction)
-      end
-
+    class Fighter < Base
       def equip(item_id)
         MUD::Logger.debug("Looking for #{item_id}.")
         new_item_id = find(item_id, types: %w[weapon armor])
@@ -70,21 +44,6 @@ module MUD
           gold: 200,
           inventory: []
         }
-      end
-
-      def starting_equipment
-        {
-          weapon: 'fists',
-          armor: 'unarmored'
-        }
-      end
-
-      def starting_room
-        1
-      end
-
-      def set_rooms_visited_to_blank
-        @rooms_visited ||= {}
       end
     end
   end
