d6a354c9dd1ca9f4d581cef70bcc01bba6633476
Luke Hill <lukehill_uk@hotmail.com>
2019-10-18 13:31:58 +0200

    Fix up v0.1.1 seed and run it

--- admin_panel/updates/v0_1_1/description.rb
+++ admin_panel/updates/v0_1_1/description.rb
@@ -2,7 +2,8 @@
 
 ap = AdminPanel::Seeder.new(:description)
 
-ap.seed(id: 1).update(
+ap.seed(
+  id: 1,
   description: 'The home of many of the men of the village of Newtown. The Newtown Pub offers its patrons a chance to
 sitdown, relax and rest your weary bones.',
   advanced_description: "The Fire is roaring, you hear chatter from the local villagers (What few reside here), about
@@ -12,7 +13,8 @@ you're not spending Gold!",
 )
 
 [2, 4, 6, 7, 8, 9].each do |id_number|
-  ap.seed(id: id_number).update(
+  ap.seed(
+    id: id_number,
     description: "The streets of Newtown are fairly quiet, as you'd expect from this remote area.",
     advanced_description: "Cobbled with primitive cut stone, and put together many years ago, these streets look like
 they've seen better times. The signs for the once active market scene are now worn, you can vaguely make out a
@@ -20,12 +22,14 @@ sign for \"Draz'ghun\" the Potion Salesman."
   )
 end
 
-ap.seed(id: 2).update(
+ap.seed(
+  id: 2,
   description: "The streets of Newtown are fairly quiet, as you'd expect from this remote area. You notice a side-path
 that leads to what looks like a Residential Area."
 )
 
-ap.seed(id: 3).update(
+ap.seed(
+  id: 3,
   description: "Draz'ghun's Shop. Whilst small in nature, the vendor here can offer a small selection of potions
 from Draz'ghuns famed brewing.",
   advanced_description: "You look around and ask the Vendor for the Owner, who simply remarks that he's away on his
@@ -34,14 +38,16 @@ To appeal to Travellers.",
   type: 'Potion Shop'
 )
 
-ap.seed(id: 5).update(
+ap.seed(
+  id: 5,
   description: 'Gatehouse. The guard looks you up and down as you leave for the Forest Path.',
   advanced_description: 'Looking ahead, you can see the Forest Path out ahead, covered in a thin trail of
 leaves from the Trees that have been cut down for their Aurel Bark.'
 )
 
 [10, 11].each do |id_number|
-  ap.seed(id: id_number).update(
+  ap.seed(
+    id: id_number,
     description: 'This secluded area is notorious for being prime looting territory for local Bandits.',
     advanced_description: 'The path is covered in a thin layer of leaves and shavings of Aurel Bark, an oft
 harvested ingredient by workers that used to be sold in Newtown.',
@@ -50,7 +56,8 @@ harvested ingredient by workers that used to be sold in Newtown.',
   )
 end
 
-ap.seed(id: 12).update(
+ap.seed(
+  id: 12,
   description: 'This clearing appears to be some sort of crossroads, the sign you can barely make out.
 You can see to your West that there is a long and windy path, which looks worn.',
   advanced_description: 'The path is covered in a thin layer of leaves and shavings of Aurel Bark, an oft
@@ -60,7 +67,8 @@ but can only see at the bottom that there is mention of a Reward?',
   location_id: 'forest_path'
 )
 
-ap.seed(id: 13).update(
+ap.seed(
+  id: 13,
   description: "The Barracks are dimly lit and the room you're in is very cold.",
   advanced_description: "As you look around, you see the light casting a shadow down the tunnel from the path
 you've just entered from. The walls have various weapons hung up, but all are broken beyond use.",
@@ -69,7 +77,8 @@ you've just entered from. The walls have various weapons hung up, but all are br
 )
 
 (14..18).to_a.each do |id_number|
-  ap.seed(id: id_number).update(
+  ap.seed(
+    id: id_number,
     description: "The Barracks are dimly lit and the room you're in is very cold.",
     advanced_description: "Looking around, you can tell that no traveller has ventured into these abandoned Barracks
 for a long time. There are some stains on the walls, but they're too dark to be blood, it almost looks ... inhuman.",
@@ -79,7 +88,8 @@ for a long time. There are some stains on the walls, but they're too dark to be 
   )
 end
 
-ap.seed(id: 19).update(
+ap.seed(
+  id: 19,
   description: "The Barracks are dimly lit and the room you're in is very cold... But you can feel some heat
 radiating from the door.",
   advanced_description: 'You notice that the style of the area is all the same, except for the fact there is a door to
@@ -90,7 +100,8 @@ the rust that has built up.',
   ktp: true
 )
 
-ap.seed(id: 20).update(
+ap.seed(
+  id: 20,
   description: 'The room is covered in blood, stained from the battles of past. There is a worn stool in the corner,
 where it looks as if the Barracks Master has been sitting.',
   advanced_description: "You've heard the stories since you were a child about some sort of Monster guarding the
