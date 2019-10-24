1c5ab93fec6e10fb7550e7961361f1856657c86e
Luke Hill <lukehill_uk@hotmail.com>
2019-10-24 14:02:30 +0200

    Slightly tweak support files, remove unrelated test code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 
 require 'rspec'
 require_relative '../app/game'
-require_relative 'support/version_scripts/file'
+require_relative 'support/file'
 require_relative 'support/console'
 
 RSpec.configure do |config|
@@ -10,7 +10,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.include SpecSupport::VersionScripts::File
+  config.include SpecSupport::File
   config.include SpecSupport::Console
 
   config.add_formatter :documentation
---
+++ spec/support/file.rb
@@ -0,0 +1,27 @@
+module SpecSupport
+  module File
+    require 'securerandom'
+
+    def read_test_screen_logs
+      ::File.read(file_location)
+    end
+
+    def log_lines
+      read_test_screen_logs.split("\n")
+    end
+
+    def switch_logging_to_temp_file
+      MUD::Screen.output('BLANK')
+      ENV['TEST_LOG_NAME'] = SecureRandom.uuid
+      MUD::Screen.instance_variable_get(:@logger).reopen("log/#{ENV['TEST_LOG_NAME']}.log")
+    end
+
+    def remove_test_screen_logs
+      ::File.delete(file_location)
+    end
+
+    def file_location
+      "log/#{ENV['TEST_LOG_NAME']}.log"
+    end
+  end
+end
