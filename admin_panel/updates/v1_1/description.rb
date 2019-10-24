e5b391833d26cea8e02bff5f91e17ad1d202a7cc
Luke Hill <lukehill_uk@hotmail.com>
2019-10-24 13:39:08 +0200

    Minor formatting fixes and run seed v1.1 on codebase and add values to db

--- admin_panel/updates/v1_1/description.rb
+++ admin_panel/updates/v1_1/description.rb
@@ -9,8 +9,8 @@ ap = AdminPanel::Seeder.new(:description)
   ap.seed(
     id: id_number,
     description: 'This secluded area is notorious for being prime looting territory for local Bandits.',
-    advanced_description: 'The path is covered in a thin layer of leaves and shavings of Aurel Bark, an oft
-harvested ingredient by workers that used to be sold in Newtown.',
+    advanced_description: 'The path is covered in a thin layer of leaves and shavings of Aurel Bark, an oft '\
+'harvested ingredient by workers that used to be sold in Newtown.',
     location_id: 'forest_path',
     type: 'Outside'
   )
@@ -19,10 +19,10 @@ end
 (51..57).each do |id_number|
   ap.seed(
     id: id_number,
-    description: 'This secluded area is notorious for being prime looting territory for local Bandits. You notice a
-lot of large structures to the North, that seems to form a large town or city.',
-    advanced_description: 'The path seems to be covered in less Aurel Bark than earlier. Indicating that this path
-is perhaps used more. You can also hear what sounds like villagers, most likely from the neighbouring Sovereign City.',
+    description: 'This secluded area is notorious for being prime looting territory for local Bandits. You notice a '\
+'lot of large structures to the North, that seems to form a large town or city.',
+    advanced_description: 'The path seems to be covered in less Aurel Bark than earlier. Indicating that this path '\
+'is perhaps used more. You can also hear what sounds like villagers, most likely from the neighbouring Sovereign City.',
     location_id: 'forest_path',
     type: 'Outside'
   )
@@ -31,10 +31,10 @@ end
 [50, 58].each do |id_number|
   ap.seed(
     id: id_number,
-    description: "This secluded area is notorious for being prime looting territory for local Bandits. You've happened
-on one of the inevitable dead-ends. Usually used to ambush travellers, you prepare for the worst.",
-    advanced_description: 'This area seems to be covered in rags, as if the Bandits wanted to keep the path clean, to
-lure even more un-suspecting travellers into it.',
+    description: "This secluded area is notorious for being prime looting territory for local Bandits. You've "\
+"happened on one of the inevitable dead-ends. Usually used to ambush travellers, you prepare for the worst.",
+    advanced_description: 'This area seems to be covered in rags, as if the Bandits wanted to keep the path clean, '\
+'to lure even more un-suspecting travellers into it.',
     location_id: 'forest_path',
     room_type: 'Outside'
   )
@@ -42,19 +42,20 @@ end
 
 ap.seed(
   id: 59,
-  description: 'The guards at this gatehouse protect this city well, judging by how pristine everything looks. The city
-has an air of arrogance about it and the Guards do nothing to quell that, dressed in the finest Steel Chainmail.',
-  advanced_description: 'Looking ahead you can make out the Council Square, where most of the Cities business is ran
-from. There are no signs directing you anywhere, almost as if everyone in the city knows where everything is.',
+  description: 'The guards at this gatehouse protect this city well, judging by how pristine everything looks. '\
+'The city has an air of arrogance about it and the Guards do nothing to quell that, dressed in the '\
+'finest Steel Chainmail.',
+  advanced_description: 'Looking ahead you can make out the Council Square, where most of the Cities business is ran '\
+'from. There are no signs directing you anywhere, almost as if everyone in the city knows where everything is.',
   location_id: 'sovereign_city_gatehouse',
 )
 
 ap.seed(
   id: 60,
-  description: "You see 3 separate small patches of ground, each monitored closely by a burly guard. He reminds you
-to read the sign before continuing if you're not sure.",
-  advanced_description: 'You notice a sign that says North: Goblin Training Arena, West: Brawler Training Arena,
-South: Under Construction.',
+  description: "You see 3 separate small patches of ground, each monitored closely by a burly guard. He reminds you "\
+"to read the sign before continuing if you're not sure.",
+  advanced_description: 'You notice a sign that says North: Goblin Training Arena, West: Brawler Training Arena, '\
+'South: Under Construction.',
   location_id: 'newtown_training_area'
 )
 
@@ -62,8 +63,8 @@ South: Under Construction.',
   ap.seed(
     id: id_number,
     description: 'This Basic Arena is designed to get novice combatants ready to leave Newtown.',
-    advanced_description: 'A Guard is in the corner, just in case things get out of hand. You see a Portcullis which
-can release monsters one at a time.',
+    advanced_description: 'A Guard is in the corner, just in case things get out of hand. You see a Portcullis which '\
+'can release various adversaries one at a time.',
     location_id: 'newtown_training_area'
   )
 end
--- admin_panel/updates/v1_1/enemy.rb
+++ admin_panel/updates/v1_1/enemy.rb
@@ -5,7 +5,8 @@ ap = AdminPanel::Seeder.new(:enemy)
 ap.seed(
   id: 'bandit',
   name: 'Bandit',
-  description: 'The Bandit is a typical ruffian. Skulking in the shadows. He preys on targets roaming the Forest Path.',
+  description: 'The Bandit is a typical ruffian. Skulking in the shadows, he preys on targets like you that roam '\
+'outside the safety of any town gates, ready to plunder whatever you may be carrying.',
   weapon_id: 'ivory_dagger',
   armor_id: 'bandit_leathers',
   lower_hp_limit: 28,
@@ -21,8 +22,8 @@ ap.seed(
 ap.seed(
   id: 'wolf',
   name: 'Wolf',
-  description: "This Wolf is unlike the normal Rabid Wolves you see floating around. Its fangs are dripping with blood,
-and it eyes you up... Like you're its next meal!",
+  description: "This Wolf is unlike the normal Rabid Wolves you see floating around. Its fangs are dripping with "\
+"blood, and it eyes you up... Like you're its next meal!",
   weapon_id: 'sharp_fangs',
   armor_id: 'wolf_hide',
   lower_hp_limit: 85,
