717dc203e02e176e60ccb2ff75fb89ffbc558f5a
Luke Hill <lukehill_uk@hotmail.com>
2019-10-30 09:47:02 +0100

    Refactor to seed descriptions and Seeder class based off mini-playtest

--- app/helpers/item.rb
+++ app/helpers/item.rb
@@ -17,11 +17,6 @@ module MUD
         inventory.detect { |item| item.name == 'Barracks Key' }
       end
 
-      def missing_barracks_key
-        'You are missing the key to the door that is blocking your passage. There are faint markings '\
-'underneath the keyhole that closely resemble the insignia of the Barracks'
-      end
-
       def gold_description
         'Glistening in your hand, these gold coins are trade-able at just about any shop'
       end
