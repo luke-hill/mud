94956391611469fb16bb3afecb4958bcc5e391b7
Luke Hill <lukehill_uk@hotmail.com>
2019-11-05 17:45:29 +0100

    Code tidy and sorting to ensure the shared examples can be loaded in from an alternate file
    Move support code to support helper context

--- spec/mud/actions/move_spec.rb
+++ spec/mud/actions/move_spec.rb
@@ -20,12 +20,4 @@ RSpec.describe MUD::Actions::Move do
     let(:direction) { 'south'}
     include_examples "Movement examples"
   end
-
-  def current_room_id
-    MUD::Game.player.current_room.room_id
-  end
-
-  def reset_room
-    MUD::Game.player.current_room = MUD::Room.new(1)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,9 +15,11 @@ require 'rspec'
 require 'delegate_matcher'
 
 require_relative '../app/game'
-require_relative 'support/file'
+
 require_relative 'support/console'
 require_relative 'support/factory'
+require_relative 'support/file'
+require_relative 'support/room'
 
 require_relative 'support/shared_examples/movement'
 
@@ -26,9 +28,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.include SpecSupport::File
   config.include SpecSupport::Console
   config.include SpecSupport::Factory
+  config.include SpecSupport::File
+  config.include SpecSupport::Room
 
   config.add_formatter :documentation
 end
