9824230d0b395eb00687eef8f400789fcb2d3903
Luke Hill <lukehill_uk@hotmail.com>
2019-11-06 14:35:57 +0100

    Key specs

---
+++ spec/mud/key_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe MUD::Weapon do
+  let(:key) { create(:key, key_data) }
+  let(:key_data) do
+    {
+      name: 'Dummy Key',
+      description: 'A Description',
+      missing_message: 'Oh dear, no key!'
+    }
+  end
+
+  describe 'delegated methods' do
+    data_keys = %i[
+      name
+      description
+      missing_message
+    ]
+    data_keys.each do |data_key|
+      it "delegates calling #{data_key} on the Key class to the key data" do
+        expect(key).to receive(:key).and_call_original
+
+        key.send(data_key)
+      end
+    end
+  end
+end
