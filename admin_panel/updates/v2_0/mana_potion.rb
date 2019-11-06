0427a425f3832b34292079579b4e26a3813aaec2
Luke Hill <lukehill_uk@hotmail.com>
2019-11-06 12:05:19 +0100

    Fix up linelength cops for v2

--- admin_panel/updates/v2_0/mana_potion.rb
+++ admin_panel/updates/v2_0/mana_potion.rb
@@ -7,7 +7,9 @@ ap = AdminPanel::Seeder.new(:mana_potion)
 ap.seed(
   id: 'lesser_magic_potion',
   name: 'Lesser Magic Potion',
-  use_message: 'You open the vial of the potion, and swill the contents with vigour. The potion restores your mana by',
-  description: 'A Basic Potion. This vial of liquid "promises" to heal the users magic power a small amount',
+  use_message: 'You open the vial of the potion, and swill the contents with vigour. The potion '\
+'restores your mana by',
+  description: 'A Basic Potion. This vial of liquid "promises" to heal the users magic '\
+'power a small amount.',
   value: 5
 )
