a19daddade86ddd8ab634117186c130ec19486ef
Luke Hill <lukehill_uk@hotmail.com>
2022-07-01 16:42:00 +0100

    minor updates

--- spec/support/console.rb
+++ spec/support/console.rb
@@ -3,7 +3,7 @@
 module SpecSupport
   module Console
     def swallow_console_spam
-      allow($stdout).to receive(:write).and_return nil
+      allow($stdout).to receive(:write).and_return(nil)
     end
   end
 end
