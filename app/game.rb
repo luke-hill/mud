d0024f04d2e7679f079b898810b371fac6b8d0b3
Luke Hill <lukehill_uk@hotmail.com>
2022-07-12 15:56:44 +0100

    Update TODOs and remove a portion of the sleep delays

--- app/game.rb
+++ app/game.rb
@@ -32,22 +32,22 @@ module MUD
       end
 
       def output_diagnostic_info
-        sleep 0.5
+        sleep 0.25
         MUD::Screen.output('............................................')
+        sleep 0.25
+        MUD::Screen.output('Hello and Welcome to Adventures of avaLancHe - Working title I know!!')
         sleep 0.5
-        MUD::Screen.output('Hello and Welcome to MUD - Working title I know!!')
-        sleep 0.75
         MUD::Screen.output("Current Version #{VERSION}")
-        sleep 0.75
+        sleep 0.5
         MUD::Screen.output('For now you are a Fighter, with 25HP.').blink
       end
 
       def output_intro_to_game_info
-        sleep 0.75
+        sleep 0.5
         MUD::Screen.output(
           "Whilst you're here, let me give you the down-low. You're about to begin your quest"
         )
-        sleep 0.75
+        sleep 0.5
         MUD::Screen.output(
           'You are in Newtown, and as avaLancHe ... our intrepid explorer, you have spawned '\
 'in our village to kill Several Monsters and then the boss - The Orc!'
