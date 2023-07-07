db45a10e96f1885507e7803459b1b4b15ed5dc7d
Luke Hill <lukehill_uk@hotmail.com>
2023-07-07 17:00:53 +0100

    v2.1 typos / language fixes

--- admin_panel/v2_1/key.rb
+++ admin_panel/v2_1/key.rb
@@ -12,14 +12,14 @@ ap.seed(
 
 ap.seed(
   id: 'aurel_bark',
-  use_message: 'You hand the Ranger the Aurel Bark. He takes one look at it and remarks that '\
-               "'It's not enough', for him to work with. And he needs more ..... a lot more."
+  use_message: "You hand the Ranger the Aurel Bark. He takes one look at it and remarks that it's not enough, for him to work with. "\
+               "He needs more ..... a lot more."
 )
 
 ap.seed(
   id: 'sylvanian_ingot',
-  use_message: "You hand the Ranger the Sylvanian Ingot.... But he steps back aghast.... \"I've not seen "\
-               "this in decades! - I wouldn't know where to start... Maybe try the Smiths in Sovereign City.\""
+  use_message: "You hand the Ranger the Sylvanian Ingot.... But he steps back aghast.... \"I've not seen something like this in"\
+               "decades! - I wouldn't know where to start... Maybe try the Smiths in Sovereign City.\""
 )
 
 ap.seed(
