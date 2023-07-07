c369a03cf3310c902e0e9fed2ee4beea42816015
Luke Hill <lukehill_uk@hotmail.com>
2023-07-07 16:51:08 +0100

    v1.0 linelength fixes

--- admin_panel/updates/v1_0/weapon.rb
+++ admin_panel/updates/v1_0/weapon.rb
@@ -7,8 +7,7 @@ ap = AdminPanel::Seeder.new(:weapon)
 ap.seed(
   id: 'small_bow',
   name: 'Small Bow',
-  description: 'This bow is made out of a single, flimsy willow branch. '\
-               'Useful for hunting rabbits and not much more.',
+  description: 'This bow is made out of a single, flimsy willow branch. Useful for hunting rabbits and not much more...',
   min_power: 3,
   max_power: 4
 )
