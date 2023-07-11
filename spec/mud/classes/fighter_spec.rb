56cb79be29a561ee3afd28ccaa2499f7a8687a77
Luke Hill <lukehill_uk@hotmail.com>
2023-07-11 18:40:09 +0100

    Partial improvement to Max Memoization restriction

--- spec/mud/classes/fighter_spec.rb
+++ spec/mud/classes/fighter_spec.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe MUD::Classes::Fighter do
-  let(:name) { subject.name }
-  let(:max_hp) { subject.max_hp }
-  let(:hp) { subject.hp }
-  let(:stamina) { subject.stamina }
-  let(:level) { subject.level }
-  let(:experience) { subject.experience }
-  let(:gold) { subject.gold }
-  let(:inventory) { subject.inventory }
-  let(:max_inventory_size) { subject.max_inventory_size }
-  let(:mp) { subject.mp }
-  let(:max_mp) { subject.max_mp }
-  let(:strength) { subject.strength }
-  let(:agility) { subject.agility }
-  let(:resilience) { subject.resilience }
-  let(:conjuring) { subject.conjuring }
   let(:attributes_quantity) { subject.instance_variable_get(:@attributes).length }
 
   describe 'initial starting stats' do
@@ -24,63 +9,63 @@ RSpec.describe MUD::Classes::Fighter do
     end
 
     it 'has a name' do
-      expect(name).to eq('Test Player')
+      expect(subject.name).to eq('Test Player')
     end
 
     it 'has 25 max hp' do
-      expect(max_hp).to eq(25)
+      expect(subject.max_hp).to eq(25)
     end
 
     it 'has 25 hp' do
-      expect(hp).to eq(25)
+      expect(subject.hp).to eq(25)
     end
 
     it 'has 1 stamina' do
-      expect(stamina).to eq(1)
+      expect(subject.stamina).to eq(1)
     end
 
     it 'is level 1' do
-      expect(level).to eq(1)
+      expect(subject.level).to eq(1)
     end
 
     it 'has no initial experience' do
-      expect(experience).to eq(0)
+      expect(subject.experience).to eq(0)
     end
 
     it 'has 200 gold' do
-      expect(gold).to eq(200)
+      expect(subject.gold).to eq(200)
     end
 
     it 'has no initial items' do
-      expect(inventory).to eq([])
+      expect(subject.inventory).to eq([])
     end
 
     it 'has 10 inventory slots' do
-      expect(max_inventory_size).to eq(10)
+      expect(subject.max_inventory_size).to eq(10)
     end
 
     it 'has 0 mp' do
-      expect(mp).to eq(0)
+      expect(subject.mp).to eq(0)
     end
 
     it 'has 0 max mp' do
-      expect(max_mp).to eq(0)
+      expect(subject.max_mp).to eq(0)
     end
 
     it 'has 10 strength' do
-      expect(strength).to eq(10)
+      expect(subject.strength).to eq(10)
     end
 
     it 'has 7 agility' do
-      expect(agility).to eq(7)
+      expect(subject.agility).to eq(7)
     end
 
     it 'has 8 resilience' do
-      expect(resilience).to eq(8)
+      expect(subject.resilience).to eq(8)
     end
 
     it 'has 5 conjuring' do
-      expect(conjuring).to eq(5)
+      expect(subject.conjuring).to eq(5)
     end
   end
 end
