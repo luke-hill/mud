7540f4b6528d1a82817f313c5df69b2adec11352
Luke Hill <lukehill_uk@hotmail.com>
2023-07-13 20:47:31 +0100

    Improve NestedGroups cop for presenter specs

--- spec/mud/presenters/directions_spec.rb
+++ spec/mud/presenters/directions_spec.rb
@@ -1,92 +1,43 @@
 # frozen_string_literal: true
 
 RSpec.describe MUD::Presenters::Directions do
-  let(:presenter) { described_class.new(double) }
   let(:visible_directions) { directions - hidden_directions }
-
-  before do
-    allow(presenter).to receive(:directions).and_return(directions)
-    allow(presenter).to receive(:hidden_directions).and_return(hidden_directions)
-  end
+  let(:trapped_room_presenter) { described_class.new('trapped_room') }
+  let(:corridor_presenter) { described_class.new('corridor') }
+  let(:open_room_presenter) { described_class.new('blank_room') }
 
   describe '#string' do
     context 'with no hidden directions' do
-      let(:hidden_directions) { [] }
-
-      context 'with no visible directions' do
-        let(:directions) { [] }
-
-        it 'shows no directions' do
-          expect(presenter.string).to eq('You cannot move in any direction')
-        end
+      it 'can show no directions' do
+        expect(trapped_room_presenter.string).to eq('You cannot move in any direction')
       end
 
-      context 'with 1 visible direction' do
-        let(:directions) { %w[north] }
-
-        it 'calls `#visible_directions_as_string`' do
-          expect(presenter).to receive(:visible_directions_as_string)
-
-          presenter.string
-        end
-
-        it 'shows only the visible directions' do
-          visible_directions.each do |direction|
-            expect(presenter.string).to include(direction)
-          end
-        end
+      it 'can show a single direction when visible' do
+        expect(corridor_presenter.string).to eq('You can only go north')
       end
 
-      context 'with 2 visible directions' do
-        let(:directions) { %w[north south] }
-
-        it 'calls `#visible_directions_as_string`' do
-          expect(presenter).to receive(:visible_directions_as_string)
-
-          presenter.string
-        end
-
-        it 'shows only the visible directions' do
-          visible_directions.each do |direction|
-            expect(presenter.string).to include(direction)
-          end
-        end
+      it 'can show multiple directions when visible' do
+        expect(open_room_presenter.string).to eq('You can go north, east, south and west')
       end
     end
 
-    context 'with 1 hidden direction' do
-      let(:hidden_directions) { %w[north] }
-
-      context 'with no visible directions' do
-        let(:directions) { [] }
-
-        it 'shows no directions' do
-          expect(presenter.string).to eq('You cannot move in any direction')
-        end
+    context 'with hidden directions' do
+      before do
+        allow(trapped_room_presenter).to receive(:hidden_directions).and_return(%w[north south])
+        allow(corridor_presenter).to receive(:hidden_directions).and_return(%w[north south])
+        allow(open_room_presenter).to receive(:hidden_directions).and_return(%w[north south])
       end
 
-      context 'with 1 visible direction' do
-        let(:directions) { %w[north] }
-
-        it 'shows no directions' do
-          expect(presenter.string).to eq('You cannot move in any direction')
-        end
+      it 'can show no directions' do
+        expect(trapped_room_presenter.string).to eq('You cannot move in any direction')
       end
 
-      context 'with 2 visible directions' do
-        let(:directions) { %w[north south] }
+      it 'shows no directions when a single direction is invisible' do
+        expect(corridor_presenter.string).to eq('You cannot move in any direction')
+      end
 
-        it 'calls `#visible_directions_as_string`' do
-          expect(presenter).to receive(:visible_directions_as_string)
-
-          presenter.string
-        end
-
-        it 'shows only the visible directions' do
-          visible_directions.each do |direction|
-            expect(presenter.string).to include(direction)
-          end
-        end
+      it 'shows only visible directions when some directions are invisible' do
+        expect(open_room_presenter.string).to eq('You can go east and west')
       end
     end
   end
