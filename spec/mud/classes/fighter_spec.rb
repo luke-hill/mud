62f206ee3516238b9fb6f651dfd6deb4414b7e9e
Luke Hill <lukehill_uk@hotmail.com>
2023-07-13 20:59:16 +0100

    Fix RSpec/NamedSubject

--- spec/mud/classes/fighter_spec.rb
+++ spec/mud/classes/fighter_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe MUD::Classes::Fighter do
-  let(:attributes_quantity) { subject.instance_variable_get(:@attributes).length }
+  subject(:hero) { described_class.new }
+
+  let(:attributes_quantity) { hero.instance_variable_get(:@attributes).length }
 
   describe 'initial starting stats' do
     it 'has 15 statistics in total' do
@@ -9,63 +11,63 @@ RSpec.describe MUD::Classes::Fighter do
     end
 
     it 'has a name' do
-      expect(subject.name).to eq('Test Player')
+      expect(hero.name).to eq('Test Player')
     end
 
     it 'has 25 max hp' do
-      expect(subject.max_hp).to eq(25)
+      expect(hero.max_hp).to eq(25)
     end
 
     it 'has 25 hp' do
-      expect(subject.hp).to eq(25)
+      expect(hero.hp).to eq(25)
     end
 
     it 'has 1 stamina' do
-      expect(subject.stamina).to eq(1)
+      expect(hero.stamina).to eq(1)
     end
 
     it 'is level 1' do
-      expect(subject.level).to eq(1)
+      expect(hero.level).to eq(1)
     end
 
     it 'has no initial experience' do
-      expect(subject.experience).to eq(0)
+      expect(hero.experience).to eq(0)
     end
 
     it 'has 200 gold' do
-      expect(subject.gold).to eq(200)
+      expect(hero.gold).to eq(200)
     end
 
     it 'has no initial items' do
-      expect(subject.inventory).to eq([])
+      expect(hero.inventory).to eq([])
     end
 
     it 'has 10 inventory slots' do
-      expect(subject.max_inventory_size).to eq(10)
+      expect(hero.max_inventory_size).to eq(10)
     end
 
     it 'has 0 mp' do
-      expect(subject.mp).to eq(0)
+      expect(hero.mp).to eq(0)
     end
 
     it 'has 0 max mp' do
-      expect(subject.max_mp).to eq(0)
+      expect(hero.max_mp).to eq(0)
     end
 
     it 'has 10 strength' do
-      expect(subject.strength).to eq(10)
+      expect(hero.strength).to eq(10)
     end
 
     it 'has 7 agility' do
-      expect(subject.agility).to eq(7)
+      expect(hero.agility).to eq(7)
     end
 
     it 'has 8 resilience' do
-      expect(subject.resilience).to eq(8)
+      expect(hero.resilience).to eq(8)
     end
 
     it 'has 5 conjuring' do
-      expect(subject.conjuring).to eq(5)
+      expect(hero.conjuring).to eq(5)
     end
   end
 end
