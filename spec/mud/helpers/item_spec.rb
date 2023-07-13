21527d8f936f1e0980447e913cb4657a0e0e755e
Luke Hill <lukehill_uk@hotmail.com>
2023-07-13 19:33:29 +0100

    Remove hard-coded weapon/item descriptions from item spec that were leaking over into other specs

--- spec/mud/helpers/item_spec.rb
+++ spec/mud/helpers/item_spec.rb
@@ -9,55 +9,15 @@ RSpec.describe MUD::Helpers::Item do
     end
   end
 
-  let(:weapon_list) do
-    {
-      'weapon_one' => {
-        'name' => 'Weapon One',
-        'description' => 'This weapon is first',
-        'min_power' => 0,
-        'max_power' => 1
-      },
-      'weapon_two' => {
-        'name' => 'Weapon Two',
-        'description' => 'This weapon is second',
-        'min_power' => 2,
-        'max_power' => 3
-      }
-    }
-  end
-
-  let(:armor_list) do
-    {
-      'armor_one' => {
-        'name' => 'Armor One',
-        'description' => 'This armor is first',
-        'defense' => 0
-      },
-      'armor_two' => {
-        'name' => 'Armor Two',
-        'description' => 'This armor is second',
-        'defense' => 2
-      }
-    }
-  end
-
   describe '#weapon_ids' do
-    before do
-      allow(item_helper).to receive(:weapon_yml).and_return(weapon_list)
-    end
-
     it 'outputs a list of the ids of all weapons that exist' do
-      expect(item_helper.weapon_ids).to eq(%w[weapon_one weapon_two])
+      expect(item_helper.weapon_ids).to be_an Array
     end
   end
 
   describe '#armor_ids' do
-    before do
-      allow(item_helper).to receive(:armor_yml).and_return(armor_list)
-    end
-
     it 'outputs a list of the ids of all armors that exist' do
-      expect(item_helper.armor_ids).to eq(%w[armor_one armor_two])
+      expect(item_helper.armor_ids).to be_an Array
     end
   end
 
