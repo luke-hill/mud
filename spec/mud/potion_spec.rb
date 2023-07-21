5ccc1e3a5c1dc6c14835c019d207b7d9f922f2d4
Luke Hill <lukehill_uk@hotmail.com>
2023-07-21 17:40:21 +0100

    Add some basic property tests for potions

--- spec/mud/potion_spec.rb
+++ spec/mud/potion_spec.rb
@@ -2,11 +2,10 @@
 
 RSpec.describe MUD::Potion do
   let(:player) { MUD::Game.player }
+  let(:potion) { create(:potion, 'demo_healing') }
 
   describe '#use' do
     context 'when the potion is a "healing" potion' do
-      let(:potion) { create(:potion, 'demo_healing') }
-
       it 'will recover some hp for the player' do
         expect(player).to receive(:hp=).once
 
@@ -33,5 +32,21 @@ RSpec.describe MUD::Potion do
         potion.use
       end
     end
+  end
+
+  it 'has a name' do
+    expect(potion.name).to eq('Demo Healing Potion')
+  end
+
+  it 'has a use_message' do
+    expect(potion.use_message).to eq('This is a demo consumption')
+  end
+
+  it 'has a description' do
+    expect(potion.description).to eq('Demo Healing Potion Description')
+  end
+
+  it 'has a value' do
+    expect(potion.value).to eq(10)
   end
 end
