315f36ab697d5c1b5347e7e8301e716ea585781a
Luke Hill <lukehill_uk@hotmail.com>
2023-07-26 18:17:08 +0100

    Make tests more robust and easier to understand

--- spec/mud/presenters/shop_items_spec.rb
+++ spec/mud/presenters/shop_items_spec.rb
@@ -3,15 +3,15 @@
 RSpec.describe MUD::Presenters::ShopItems do
   subject(:shop_items_presenter) { described_class.new(shop) }
 
-  let(:dashed_line) { /-{10,}/ }
-  let(:word_line) { /\|.+\|.+\|/ }
+  let(:dashed_regex) { /-{10,}/ }
+  let(:word_regex) { /\|.+\|.+\|/ }
 
   describe '#string' do
     context 'with 1 item for sale' do
       let(:shop) { create_shop('small_shop') }
 
       it 'will display a list formatted with just the single item' do
-        expect(shop_items_presenter.string).to match(/\A-{10,}\n\|.+\|.+\|\n-{10,}\z/)
+        expect(shop_items_presenter.string).to match(/\A#{dashed_regex}\n#{word_regex}\n#{dashed_regex}\z/)
       end
     end
 
@@ -19,7 +19,7 @@ RSpec.describe MUD::Presenters::ShopItems do
       let(:shop) { create_shop('valid_shop') }
 
       it 'will display a list formatted with all of the items' do
-        expect(shop_items_presenter.string).to match(/\A-{10,}\n\|.+\|.+\|\n-{10,}\n\|.+\|.+\|\n-{10,}\z/)
+        expect(shop_items_presenter.string).to match(/\A#{dashed_regex}\n#{word_regex}\n#{dashed_regex}\n#{word_regex}\n#{dashed_regex}\z/)
       end
     end
   end
