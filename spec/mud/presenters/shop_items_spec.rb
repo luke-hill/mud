bdb5208581179af652e620cb7c1659b029827bd3
Luke Hill <lukehill_uk@hotmail.com>
2023-07-26 18:14:59 +0100

    Remove stubs

--- spec/mud/presenters/shop_items_spec.rb
+++ spec/mud/presenters/shop_items_spec.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe MUD::Presenters::ShopItems do
-  let(:shop_items_presenter) { described_class.new(shop) }
+  subject(:shop_items_presenter) { described_class.new(shop) }
+
+  let(:dashed_line) { /-{10,}/ }
+  let(:word_line) { /\|.+\|.+\|/ }
 
   describe '#string' do
     context 'with 1 item for sale' do
       let(:shop) { create_shop('small_shop') }
 
       it 'will display a list formatted with just the single item' do
-        expect(shop_items_presenter).to receive(:dashed_string).at_most(:twice)
-
-        shop_items_presenter.string
+        expect(shop_items_presenter.string).to match(/\A-{10,}\n\|.+\|.+\|\n-{10,}\z/)
       end
     end
 
@@ -18,9 +19,7 @@ RSpec.describe MUD::Presenters::ShopItems do
       let(:shop) { create_shop('valid_shop') }
 
       it 'will display a list formatted with all of the items' do
-        expect(shop_items_presenter).to receive(:dashed_string).at_least(3).times
-
-        shop_items_presenter.string
+        expect(shop_items_presenter.string).to match(/\A-{10,}\n\|.+\|.+\|\n-{10,}\n\|.+\|.+\|\n-{10,}\z/)
       end
     end
   end
