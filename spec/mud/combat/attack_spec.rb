e51561d68b1bea73614e12e39b6ac61da5e7d784
Luke Hill <lukehill_uk@hotmail.com>
2023-07-26 19:12:15 +0100

    Fix up some styling and some rubocop complexity offenses (partially)

--- spec/mud/combat/attack_spec.rb
+++ spec/mud/combat/attack_spec.rb
@@ -1,36 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe MUD::Combat::Attack do
-  subject(:attack_instance) { described_class.new(enemy) }
+  subject(:attack_attempt) { attack_instance.attack }
 
+  let(:attack_instance) { described_class.new(enemy) }
+  let(:attack_message_regex) { /You hit the #{enemy_name} with your #{weapon_name} for \d+ damage./ }
+  let(:enemy) { create(:enemy, 'bad') }
+  let(:enemy_name) { 'TEST - Bad Enemy' }
+  let(:missed?) { false }
+  let(:missed_message) { "You tried to attack the #{enemy_name} with your #{weapon_name}... but missed.".yellow }
   let(:player) { MUD::Game.player }
   let(:weapon) { MUD::Weapon.of_type('zero') }
   let(:weapon_name) { weapon.name }
-  let(:enemy_name) { 'TEST - Bad Enemy' }
-  let(:enemy) { create(:enemy, 'bad') }
+
+  before do
+    allow(player).to receive(:weapon).and_return(weapon)
+    allow(attack_instance).to receive(:missed?).and_return(missed?)
+    switch_logging_to_temp_file
+  end
+
+  after { remove_test_screen_logs }
 
   describe '#attack' do
-    subject(:attack_attempt) { attack_instance.attack }
-
-    let(:missed?) { false }
-    let(:missed_message) { "You tried to attack the #{enemy_name} with your #{weapon_name}... but missed.".yellow }
-    let(:attack_message_regex) do
-      /You hit the #{enemy_name} with your #{weapon_name} for \d+ damage./
-    end
-
-    before do
-      allow(player).to receive(:weapon).and_return(weapon)
-      allow(attack_instance).to receive(:missed?).and_return(missed?)
-      switch_logging_to_temp_file
-    end
-
-    after { remove_test_screen_logs }
-
     context "when the player's attack misses" do
       let(:missed?) { true }
 
       it 'informs the player that the attack attempt missed' do
-        expect(attack_attempt).to match("You tried to attack the #{enemy_name} with your #{weapon_name}... but missed.".yellow)
+        expect(attack_attempt).to eq("You tried to attack the #{enemy_name} with your #{weapon_name}... but missed.".yellow)
       end
     end
 
