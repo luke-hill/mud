a0f71cd24f3cb98a0a9917ea0ca9a55a2fa6f7d2
Luke Hill <lukehill_uk@hotmail.com>
2023-07-28 17:51:02 +0100

    Add ktp movement tests

--- spec/mud/actions/move_spec.rb
+++ spec/mud/actions/move_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe MUD::Actions::Move do
+  subject(:move_attempt) { described_class.new(direction).move }
+
+  let(:player) { MUD::Game.player }
+
   describe '#north' do
     let(:direction) { 'north' }
 
@@ -35,5 +39,21 @@ RSpec.describe MUD::Actions::Move do
     let(:direction) { 'down' }
 
     include_examples 'Movement examples'
+  end
+
+  context 'when the room has a ktp enemy still alive' do
+    let(:direction) { 'north' }
+
+    before { player.current_room = MUD::Room.new('enemy_ktp_room') }
+
+    it 'does not let the player leave the room' do
+      expect { move_attempt }.not_to(change { current_room_id })
+    end
+
+    it 'informs the player you must kill the present enemy before leaving' do
+      expect(MUD::Screen).to receive(:output).with("You must kill the #{player.current_room.enemy.name} before leaving the room!".red)
+
+      move_attempt
+    end
   end
 end
