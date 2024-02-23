612ffbae2bcf1f13e6b888cd2977210d96463c3b
Luke Hill <lukehill_uk@hotmail.com>
2024-02-23 16:28:54 +0000

    Reduce the stubbing on defense class to test more of the class

--- spec/mud/combat/defend_spec.rb
+++ spec/mud/combat/defend_spec.rb
@@ -3,16 +3,12 @@
 RSpec.describe MUD::Combat::Defend do
   subject(:defend_attempt) { defend_instance.defend }
 
-  let(:damage_taken) { 2 }
   let(:defend_instance) { described_class.new(enemy) }
   let(:enemy) { create(:enemy, 'bad') }
   let(:player) { MUD::Game.player }
   let(:weapon_name) { MUD::Weapon.of_type(enemy.weapon_id).name }
 
-  before do
-    allow(defend_instance).to receive_messages(damage_taken:, missed?: false)
-    player.hp = player.max_hp
-  end
+  before { player.hp = player.max_hp }
 
   describe '#defend' do
     context 'when the enemies attack misses' do
@@ -26,6 +22,8 @@ RSpec.describe MUD::Combat::Defend do
     end
 
     context 'when the player takes no damage' do
+      before { allow(defend_instance).to receive_messages(damage_taken:, missed?: false) }
+
       let(:damage_taken) { 0 }
 
       it 'informs the player that the enemy attack attempt missed' do
@@ -35,23 +33,29 @@ RSpec.describe MUD::Combat::Defend do
       end
     end
 
-    it 'informs the player that the attack dealt damage' do
-      defend_attempt
+    context 'when the player takes some damage' do
+      before { allow(defend_instance).to receive_messages(attack_value: 2, defense_value: 0, missed?: false) }
 
-      expect(log_lines).to include(/The #{enemy.name} hit you with its #{weapon_name} for #{damage_taken} damage./)
-    end
+      it 'informs the player that the attack dealt damage' do
+        defend_attempt
 
-    it 'reduces the players hp by the amount of damage dealt' do
-      expect { defend_attempt }.to change(MUD::Game.player, :hp).by(-2)
-    end
+        expect(log_lines).to include(/The #{enemy.name} hit you with its #{weapon_name} for 2 damage./)
+      end
 
-    it 'sends a debug statement with the players new hp value' do
-      defend_attempt
+      it 'reduces the players hp by the amount of damage dealt' do
+        expect { defend_attempt }.to change(MUD::Game.player, :hp).by(-2)
+      end
 
-      expect(log_lines).to include(/DEBUG --> YOUR HP:\d+hp./)
+      it 'sends a debug statement with the players new hp value' do
+        defend_attempt
+
+        expect(log_lines).to include(/DEBUG --> YOUR HP:\d+hp./)
+      end
     end
 
     context 'when the attack kills the player' do
+      before { allow(defend_instance).to receive_messages(damage_taken:, missed?: false) }
+
       let(:damage_taken) { 25 }
 
       it 'does not send a debug statement with the enemies new hp value' do
